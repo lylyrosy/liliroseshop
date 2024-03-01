abf19eb1e856a2476e178e9b76fa9c0febbb2df7
lylyrosy <lylyrose51@gmail.com>
2024-03-01 16:33:05 +0100

    mmmmmmmmmmmmmmm

--- src/pages/bois.jsx
+++ src/pages/bois.jsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import BasicTable from '../composants/BasicTable';
 import Typography from '@mui/material/Typography'
-
+import BrowserUpdatedIcon from '@mui/icons-material/BrowserUpdated';
+import InstallMobileIcon from '@mui/icons-material/InstallMobile';
 import { useTheme } from '@emotion/react';
 const Bois = () => {
 
@@ -21,10 +22,19 @@ const Bois = () => {
             <Typography color={theme.palette.appco.main } mt='30px' display='flex' justifyContent='center' component="h6"> <b>Hydrate-toi, c'est ta superpuissance !</b>  </Typography>
        
        
-            <Typography color={theme.palette.magie.main } display='flex' justifyContent='center'  mt='100px' fontSize='15px' componentt="code">Designed by Lili Rose</Typography>
-       <Typography   sx={{   fontFamily: "Gruppo, sans-serif", fontWeight: 400, fontStyle:'normal'}}  display='flex' justifyContent='center'  mt='0px' fontSize='10px' componentt="code">mon kif pour l'informatique m'accompagne sur le chemin qui mène à la paix</Typography>
+            <Typography color={theme.palette.magie.main } display='flex' justifyContent='center'  mt='20px' fontSize='15px' componentt="code">Designed by Lili Rose</Typography>
+            
+            
+            <Typography color={theme.palette.appco.main } display='flex' justifyContent='center'  fontSize='12px' componentt="code">Trouvez dans la barre d'adresse Installer l'application </Typography>
 
-        
+            <Typography color={theme.palette.appco.main } display='flex' justifyContent='center'  fontSize='12px' componentt="code"> Lili Rose-Shop <BrowserUpdatedIcon/> ou <InstallMobileIcon/>  </Typography>
+
+
+
+       <Typography  color={theme.palette.appco.main } sx={{   fontSize:'12px', fontFamily: "Gruppo, sans-serif", fontWeight: 400, fontStyle:'normal'}}  display='flex' justifyContent='center'  mt='0px' fontSize='10px' >mon kiff pour l'informatique </Typography>
+
+       <Typography  color={theme.palette.appco.main } sx={{  fontSize:'12px', fontFamily: "Gruppo, sans-serif", fontWeight: 400, fontStyle:'normal'}}  display='flex' justifyContent='center'  mt='0px' fontSize='10px'> m'accompagne sur le chemin qui mène à la paix</Typography>
+
         </div>
 
     );
